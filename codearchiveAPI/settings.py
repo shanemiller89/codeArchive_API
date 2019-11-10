caff022654b74436c832a882282d7bd71e2e390e
Shane Miller <shanemiller89@gmail.com>
2019-11-10 16:28:00 -0600

    allowed fix

--- codearchiveAPI/settings.py
+++ codearchiveAPI/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9xpg@7+tabej@p$wv^xanew#j9eq!&&$#93r1mwym+$7wc4-@m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS += ['api.codearchive.net', '127.0.0.1:8000']
 
 
 # Application definition
