d395dd50ee348bd3d3d7f5315829110be6fd332c
Shane Miller <shanemiller89@gmail.com>
2019-11-10 16:19:57 -0600

    fix

--- codearchiveAPI/settings.py
+++ codearchiveAPI/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9xpg@7+tabej@p$wv^xanew#j9eq!&&$#93r1mwym+$7wc4-@m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
