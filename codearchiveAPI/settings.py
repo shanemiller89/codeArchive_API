941db6521ff0b97fa25ce9a7d1584b44d426f23e
Shane Miller <shanemiller89@gmail.com>
2019-11-11 15:46:06 -0600

    debug off

--- codearchiveAPI/settings.py
+++ codearchiveAPI/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9xpg@7+tabej@p$wv^xanew#j9eq!&&$#93r1mwym+$7wc4-@m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['api.codearchive.net', '127.0.0.1:8000']
 
